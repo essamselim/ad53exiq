0539c46e6a3d0c1124ac9ece9074022dbc9cc8c5
essamselim <52804085+essamselim@users.noreply.github.com>
2021-11-14 23:38:44 +0200

    Update function.py

--- ad53exiq/function.py
+++ ad53exiq/function.py
@@ -5,6 +5,4 @@ from PIL import Image
 def imshow(X, resize=None,nR=30,nC=30):
         number_columns, number_rows = X.shape[0], X.shape[1]
         return [[X[int(number_rows * r / nR)][int(number_columns * c / nC)]for c in range(nC)] for r in range(nR)]
-X=np.random.random((50,50))
-c=imshow(X)
-print(np.shape(c))
+
